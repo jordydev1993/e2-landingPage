a90ebf5b09d884e193d2b5a2b450c351bcf55ae5
jordydev1993 <jordydev.1993@gmail.com>
2024-07-09 01:49:12 -0300

    add head2

--- src/contexts/MenuContext.jsx
+++ src/contexts/MenuContext.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // src/contexts/MenuContext.jsx
 import React, { createContext, useState } from 'react';
 
@@ -13,19 +13,6 @@ export const MenuProvider = ({ children }) => {
     </MenuContext.Provider>
   );
 };
-=======
-// src/contexts/MenuContext.jsx
-import React, { createContext, useState } from 'react';
 
-export const MenuContext = createContext();
 
-export const MenuProvider = ({ children }) => {
-  const [showMenu, setShowMenu] = useState(false);
 
-  return (
-    <MenuContext.Provider value={{ showMenu, setShowMenu }}>
-      {children}
-    </MenuContext.Provider>
-  );
-};
->>>>>>> 85c60de07607060d06e6755924203e37dcb36ed0
