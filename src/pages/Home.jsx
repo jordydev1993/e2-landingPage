ffdae97a873e5882317cb89ae91f27ba06c3da64
jordy <jordy.10.garcia@gmail.com>
2024-07-09 01:28:47 -0300

    add head

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // src/pages/Home.jsx
 import React from 'react';
 import Hero from '../components/Hero';
@@ -20,26 +20,6 @@ const Home = () => {
 };
 
 export default Home;
-=======
-// src/pages/Home.jsx
-import React from 'react';
-import Hero from '../components/Hero';
-import Clients from '../components/Clients';
-import Works from '../components/Works';
-import Reviews from '../components/Reviews';
-import Services from '../components/Services';
 
-const Home = () => {
-  return (
-    <div>
-      <Hero />
-      <Clients />
-      <Works />
-      <Reviews />
-      <Services />
-    </div>
-  );
-};
 
-export default Home;
->>>>>>> 85c60de07607060d06e6755924203e37dcb36ed0
+
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from "react";
 
 const Products = () => {
@@ -76,20 +76,3 @@ const Products = () => {
 };
 
 export default Products;
-=======
-// src/pages/Products.jsx
-import React from 'react';
-
-const Products = () => {
-  return (
-    <section className="min-h-[90vh] p-8">
-      <h1 className="text-4xl font-bold">Productos</h1>
-      <p className="text-xl text-gray-500 mt-4">
-        Lista de productos que ofrecemos.
-      </p>
-    </section>
-  );
-};
-
-export default Products;
->>>>>>> 85c60de07607060d06e6755924203e37dcb36ed0
